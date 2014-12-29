dde072babbec3752edfcdd869c682961ed810031
estelle2501 <magda.mirowicz@gmail.com>
2014-12-29 15:42:55 +0100

    1.0

---
+++ SimpleAppAnnotation/src/main/java/com/hibernate/tutorial/Main.java
@@ -0,0 +1,28 @@
+package com.hibernate.tutorial;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.hibernate.persistence.HibernatePersistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Session session = HibernatePersistence.getSessionFactory()
+				.openSession();
+
+		session.beginTransaction();
+
+		Query query = session.createQuery("from ZipCodes");
+
+		List<ZipCodes> zipCodesList = query.list();
+		for (ZipCodes z : zipCodesList) {
+			System.out.println(+z.getId() + "," + z.getZipCode());
+		}
+
+		// close session
+		HibernatePersistence.shutdown();
+	}
+}
